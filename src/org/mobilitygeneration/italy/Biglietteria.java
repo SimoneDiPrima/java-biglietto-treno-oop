32f79b29c09cbe0916aff0c593aa7654ce4f2971
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-11-21 15:04:58 +0100

    second commit

---
+++ src/org/mobilitygeneration/italy/Biglietteria.java
@@ -0,0 +1,29 @@
+package org.mobilitygeneration.italy;
+
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class Biglietteria {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("quanti km vuoi percorrere??");
+		int kilometers = sc.nextInt();
+		System.out.print("quanti anni hai??");
+		int age = sc.nextInt();
+		
+		try {
+			
+			Biglietto p = new Biglietto(kilometers, age);
+			System.out.print(p);	
+			
+		} catch(Exception e) {
+			
+			System.err.println(e.getMessage());
+		}
+			
+	}
+	
+}
--- src/org/mobilitygeneration/italy/Biglietto.java
+++ src/org/mobilitygeneration/italy/Biglietto.java
@@ -3,24 +3,31 @@ package org.mobilitygeneration.italy;
 public class Biglietto {
 	private double kilometers;
 	private int age;
-	public double calcolaPrezzo;
-	private int calcolaSconto;
+	public double prezzo = kilometers * 0.21;
+	
+
+	private double calcolaSconto;
 	public Biglietto(int kilometers,int age)throws Exception{
 		setKilometers(kilometers);
 		setAge(age);
 	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = kilometers * 0.21;
+		this.prezzo = prezzo;
+	}
 	public double getKilometers() {
 		return kilometers;
 	}
-	public double calcolaPrezzo(double kilometers,int age) {
-		return calcolaPrezzo = kilometers *0.21;
 	
-	}
 	
-	public void setKilometers(int kilometers) throws Exception {
+	public void setKilometers(double kilometers) throws Exception {
 		if(kilometers <= 0 ) {
 			throw new Exception("devi fare almeno un kilometro!");
 		}
+		this.kilometers = kilometers;
 	}
 
 	public int getAge() {
@@ -31,14 +38,23 @@ public class Biglietto {
 		if(age <= 0 ) {
 			throw new Exception("il cliente deve avere un eta di almeno un anno!");
 		}
+		this.age = age;
 	}
-	public int getCalcolaSconto() {
-		return calcolaSconto;
+	public double getCalcolaSconto(double kilometers , double age) {
+	 if(age < 18) {
+		 calcolaSconto = kilometers * 0.21 * 20;
+	 }
+	 if(age >=65 ) {
+		 calcolaSconto = kilometers * 0.21 * 40;
+	 }
+		return this.calcolaSconto = calcolaSconto;
 	}
-	public void setCalcolaSconto(int calcolaSconto) {
-		this.calcolaSconto = calcolaSconto;
-	}
+	
+	@Override
+	public String toString() {
 		
+		return  "il passeggero ha:" + age + "anni" + "\nil costo del biglietto è : " + prezzo;
+	}
 		
 	}
 
